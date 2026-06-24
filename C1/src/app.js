73ff9710ec4436c28f25f5e9f408568f0633f2bc
Matu <matias.untref@gmail.com>
2026-06-24 02:10:34 -0300

    reestructuracion de carpetas y del productmanaget con express

--- C1/src/app.js
+++ C1/src/app.js
@@ -1,11 +1,15 @@
 const express=require("express")
+const {ProductManager} = require("./dao/ProductManager.js")
 
 const PORT=3000
 
 const app=express()
 
-app.get("/products", (req, res)=>{
-    res.send("Products...")
+const productManager = new ProductManager()
+
+app.get("/products", async (req, res)=>{
+    let products = await productManager.getProducts()
+    res.send(products)
 })
 
 app.listen(PORT, ()=>{
--- C1/src/dao/ProductManager.js
+++ C1/src/dao/ProductManager.js
@@ -1,77 +1,21 @@
+const fs = require("fs")
+
 class ProductManager {
 
-    #products;
-
-    constructor(){
-        this.#products=[]
+    constructor(path="../src/data/products.json"){
+        this.path=path
     }
 
-    addProduct(title, description, price, thumbnail, code, stock){
-        if (!title || !description || !price || !thumbnail || !code || stock === undefined || stock === null) {
-            console.log("Alguno de los parametros estan faltantes y son requeridos, por favor verifique");
-            return;
-        }
-        const codes = this.#products.map(({ code }) => code);
-        if(codes.includes(code)){
-            console.log("El codigo de producto no puede repetirse");
-            return;
-       }
-
-        let id= this.#products.length+1;
-        let newProduct={
-            id,
-            title, 
-            description,
-            price,
-            thumbnail,
-            code,
-            stock
-        }
-        this.#products.push(newProduct);
-    }
-
-    getProducts(){
-        console.log(this.#products);
-        return this.#products;
-    }
-
-    getProductById(id){
-        const product = this.#products.find(prod => prod.id === id);
-        if(product){
-            console.log(product);
-            return product;
+    async getProducts(){
+        if(fs.existsSync(this.path)){
+            return JSON.parse(await fs.promises.readFile(this.path, "utf-8"))
         } else {
-            console.log("No hay producto con ese id");
-            return;
+            return []
         }
     }
+
 }
 
-const prod1=["Jabon", "muy limpio", 2, "indefinido por el momento", 123123, 1000];
-
-let productManager = new ProductManager();
-console.log("Ingreso un producto valido")
-productManager.addProduct(...prod1)
-productManager.getProducts();
-
-const prod2=["Carbon", "muy sucio", "indefinido por el momento", 123123, 1000];
-console.log("Ingreso un producto invalido")
-productManager.addProduct(...prod2)
-productManager.getProducts();
-
-const prod3=["Carbon", "muy sucio", 10, "indefinido por el momento", 123123, 1000];
-console.log("Ingreso un producto invalido por codigo duplicao")
-productManager.addProduct(...prod3)
-productManager.getProducts();
-
-const prod4=["Carbon", "muy sucio", 4, "indefinido por el momento", 123124, 1000];
-console.log("Ingreso otro producto valido")
-productManager.addProduct(...prod4)
-productManager.getProducts();
-
-console.log("Buscando prod con id 2");
-productManager.getProductById(2);
-console.log("Buscando prod con id 3");
-productManager.getProductById(3);
-console.log("Buscando prod con id 1");
-productManager.getProductById(1);
+module.exports={
+    ProductManager
+}
