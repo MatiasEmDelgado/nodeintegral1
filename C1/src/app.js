3f69049b72fb3279579cce44925edc085e3fac68
Matu <matias.untref@gmail.com>
2026-06-24 03:30:04 -0300

    Se agrega el get de producto con id

--- C1/src/app.js
+++ C1/src/app.js
@@ -5,11 +5,34 @@ const PORT=3000
 
 const app=express()
 
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
 const productManager = new ProductManager()
 
 app.get("/products", async (req, res)=>{
-    let products = await productManager.getProducts()
-    res.send(products)
+    try {
+
+        let products = await productManager.getProducts()
+        let {limit}=req.query
+        if(limit){
+            products=products.slice(0,limit)
+        }
+        res.send(products)
+    } catch (error) {
+        res.send({"error": "Internal server error"})
+    }
+})
+
+app.get("/product/:id", async (req, res)=>{
+    try {
+        let {id} = req.params
+        let product = await productManager.getProduct(id)
+        let {limit}=req.query
+        res.send(product)
+    } catch (error) {
+        res.send({"error": "Internal server error"})
+    }
 })
 
 app.listen(PORT, ()=>{
--- C1/src/dao/ProductManager.js
+++ C1/src/dao/ProductManager.js
@@ -7,11 +7,31 @@ class ProductManager {
     }
 
     async getProducts(){
-        if(fs.existsSync(this.path)){
-            return JSON.parse(await fs.promises.readFile(this.path, "utf-8"))
-        } else {
-            return []
+        try {
+            if(fs.existsSync(this.path)){
+                return JSON.parse(await fs.promises.readFile(this.path, "utf-8"))
+            } else {
+                return []
+            }
+        } catch(error) {
+            console.log(error)
         }
+    }
+
+    async getProduct(id){
+        try {
+            let products = await this.getProducts()
+            let product = products.find(prod=>prod.id==id)
+            if(product){
+                return product
+            } else {
+                //throw new Error(`Product ${id} doesnt exist`)
+                return []
+            }
+        } catch(error) {
+            console.log(error)
+        }
+
     }
 
 }
